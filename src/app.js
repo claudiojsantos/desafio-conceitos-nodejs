cd7221f122106d96d9a0bb04588a10d946c73887
Cláudio Santos <cljos@hotmail.com>
2020-04-09 21:36:16 -0300

    Configuração do Debug do VsCode e solucionando o primeiro desafio goStack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -10,24 +11,91 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryID(request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid Repository ID" })
+  }
+
+  return next()
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+  const { title, url, techs } = request.body
+  const likes = 0;
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  if (!title) {
+    response.status(400).json({ error: "Digite o título" })
+  } else if (!url) {
+    response.status(400).json({ error: "Digite a URL" })
+  } else if (!techs) {
+    response.status(400).json({ error: "Digite as tecnologias" })
+  }
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const repository = { id: uuid(), title, url, techs, likes }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(rep => rep.id === id)
+
+  if (repositoryIndex < 0) {
+    response.status(400).json({ "error": "Repository not found" })
+  }
+
+  let likes = repositories[repositoryIndex].likes
+
+  likes += 1
+
+  repositories[repositoryIndex].likes = likes
+
+  return response.json({ "likes": repositories[repositoryIndex].likes })
 });
+
+app.put("/repositories/:id", validateRepositoryID, (request, response) => {
+  const { id } = request.params
+  const { url, title, techs, likes } = request.body
+
+  if (likes) {
+    return response.status(400).json({ "likes": 0 })
+  }
+
+  const repositoryIndex = repositories.findIndex(rep => rep.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ "error": "Repository not found" })
+  }
+
+  const repository = {id, url, title, techs}
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
+});
+
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params
+
+  const repositoryIndex = repositories.findIndex(rep => rep.id === id)
+
+  if (repositoryIndex < 0) {
+    res.status(400).json({ "error": "Repository not found" })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return res.status(204).send()
+});
+
 
 module.exports = app;
